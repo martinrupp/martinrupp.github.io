8db53e306573d5f8d2f5d648dafcb9a076bb9bf5
Martin Rupp <mrupp@splicemachine.com>
2020-05-13 20:57:45 +0200

    fixed preds parsing, cleaned up code, added comments

--- tools/splice_plan/parse_splice_plan.js
+++ tools/splice_plan/parse_splice_plan.js
@@ -1,14 +1,22 @@
 function parse_node(m)
 {
+    // intend -> name ( rest )
+    // m[1] = intend
+    // m[2] = name
+    // m[3] = rest
     if( m && m.length == 4)
     {
         var node = {}
-        node.intend = m[1].length/2
+        node.intend = m[1].length/2 // splice is using 2 spaces for intend
         node.name = m[2];
         node.options = {}
         node.children = []
 
-        var m2 = m[3].match(/(\w*)=([\w. ]*)/gm);
+        // mapping rest into node.options:
+        // e.g. `outputHeapSize=488.804 MB,partitions=1`
+        // into node.options.outputHeapSize = "488.804 MB", node.options.partitions = "1"
+        // note that we scan here for word=word until
+        var m2 = m[3].match(/(\w*)=([^,]*)/gm);
         if( m2 )
         {
             for (var j = 0; j < m2.length; j++) {
@@ -22,30 +30,52 @@ function parse_node(m)
         return null;
 }
 
+function parse_first(text)
+{
+    // parse the first line without a '->', e.g.
+    // Cursor(n=6,rows=2369593,updateMode=READ_ONLY (1),engine=Spark)
+    // -> m[1] = ``
+    // -> m[2] = Cursor
+    // -> m[3] = n=6,rows=2369593,updateMode=READ_ONLY (1),engine=Spark
+    var m = text.match(/^( *)(\S*)\((.*)\)/);
+    return parse_node(m)
+}
+
+function parse_other(text)
+{
+    // parseing e.g.
+    // `     ->  ProjectRestrict(n=4,totalCost=59891.557,outputRows=2369593,outputHeapSize=488.804 MB,partitions=1)`
+    // -> m[1] = `     `
+    // -> m[2] = ProjectRestrict
+    // -> m[3] = n=4,totalCost=59891.557,outputRows=2369593,outputHeapSize=488.804 MB,partitions=1
+    var m = text.match(/^( *)->  (\S*)\((.*)\)/);
+    return parse_node(m);
+}
+
 function parse_splice_plan( text )
 {
     var lines = text.split('\n');
-    var m = lines[0].match(/^( *)(\S*)\((.*)\)/);
+    // parse first line (Cursor(...))
+    var parent, root_node = parse_first(lines[0])
 
-    var root_node = parse_node(m)
-    var current_node = root_node;
-    var stack = []
+    var stack = [root_node]
 
-
-    for(var i = 1; i < lines.length;i++){
-        // console.log("-------------- " + lines[i]);
-        var m = lines[i].match(/^( *)->  (\S*)\((.*)\)/);
-        
-        var node = parse_node(m)
+    for(var i = 1; i < lines.length;i++)
+    {
+        // parse other lines line (   ->  ProjectRestrict(n=4, ... )
+        var node = parse_other(lines[i]);
 
         if( node )
         {
-            var intend = node.intend;
-            while( current_node.intend+1 != intend )
-                current_node = stack.pop()
-            current_node.children.push( node );
-            stack.push(current_node)
-            current_node = node
+            // get "parent" node which has matching intend level
+            do {
+              parent = stack.pop()
+            }
+            while( parent.intend+1 != node.intend );
+            // add node as children to parent
+            parent.children.push( node );
+            stack.push(parent)
+            stack.push(node)
         }
     }
 
